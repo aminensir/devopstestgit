41f2915ba329ab1ac1e327f0e77e737888610a0e
atallah039 <yassine.atallah@esprit.tn>
2024-11-12 21:03:08 +0100

    Junit-test

--- src/test/java/com/example/testeditions/TestEditioNsApplicationTests.java
+++ src/test/java/com/example/testeditions/TestEditioNsApplicationTests.java
@@ -1,22 +1,5 @@
 package com.example.testeditions;
 
-<<<<<<< Updated upstream
-import com.example.testeditions.Services.PostServiceImpl;
-import com.example.testeditions.Entites.Post;
-import com.example.testeditions.Entites.User;
-import com.example.testeditions.Repositories.PostRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.Date;
-import java.util.Optional;
-
-=======
 import com.example.testeditions.Entites.TypeLocale;
 import com.example.testeditions.Repositories.AnnonceAirbnbRepository;
 import com.example.testeditions.Services.AnnAirbnbImpl;
@@ -35,45 +18,11 @@ import java.util.Date;
 import java.util.Optional;
 
 
->>>>>>> Stashed changes
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TestEditioNsApplicationTests {
     @Mock
-<<<<<<< Updated upstream
-    PostRepository postRepository;
-
-    @InjectMocks
-    PostServiceImpl postService;
-
-    Post post = new Post(1L, "Description du post", new Date(), new User(), null, null);
-
-    @Test
-    public void getAllPosts() {
-        // Exemple de structure pour les tests de récupération de tous les posts
-        // Implémentez la logique de votre test ici si nécessaire
-    }
-
-    @Test
-    public void getPostById() {
-        // Simulation de la méthode `findById` du repository
-        when(postRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(post));
-
-        // Appel du service pour obtenir le post par ID
-        Post retrievedPost = postService.getPostById(1L);
-
-        // Vérifications
-        Assertions.assertNotNull(retrievedPost);
-        Assertions.assertEquals("Description du post", retrievedPost.getDescriptionPost());
-    }
-
-    @Test
-    public void createPost() {
-        // Exemple de test pour la création d'un post (à implémenter selon votre logique métier)
-    }
-}
-=======
     AnnonceAirbnbRepository annonceAirbnbRepository;
 
     @InjectMocks
@@ -107,4 +56,3 @@ class TestEditioNsApplicationTests {
     }
 
 }
->>>>>>> Stashed changes
